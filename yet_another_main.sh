8353369826c52f3c1800f870a0b38048bb2a64ac
Elad Karako <eladkarako@gmail.com>
2014-03-19 12:46:21 +0200

    add sleep while file not exist for less then 1 second

--- yet_another_main.sh
+++ yet_another_main.sh
@@ -3,3 +3,9 @@ date +"%T.%3N"
 it shows the time with nanoseconds in 3 digits.
 example: 20:57:11.718
 '
+
+
+while [ ! -f BI.iso ]; do sleep 0.4s; echo "$(date +"%T.%3N") waiting for file.."; done;
+'
+sleep while file not exist, sleep for 400 milliseconds. show time.
+'
